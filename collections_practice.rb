3fc0d22be2c3a1d211b389469a65e6455895962a
Tanya Bihag <tbihag@gmail.com>
2019-12-19 03:07:53 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array 
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array) 
+  array.each do |i|
+    i[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |i|
+    i.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index {|x, i| x << "s" unless i == 1}
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
